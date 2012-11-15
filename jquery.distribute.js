4147f2985fa253e3897c4e0add04659895693087
Luke Stevenson <luke@lucanos.com>
2012-11-15 13:15:18 +1100

    Making Progress. Still a little buggy.

--- jquery.distribute.js
+++ jquery.distribute.js
@@ -8,35 +8,88 @@
  */
 
 (function($){
- $.fn.distribute = function(options) {
 
-  var defaults = {
-   margin: 10,
-   width: null
+  $.fn.distribute = function(options){
+
+    var defaults = {
+     margin: 10,
+     width: null
+    };
+    var options = $.extend(defaults, options);
+
+    return this.each(function(){
+
+      var $parent = $(this);
+      var totalWidth = $parent.width(); 
+      var itemWidths = new Array();
+      var itemWidthsWithMargin = new Array();
+      var $items = $parent.children();
+
+      var rowContentsWidth = 0;
+      var $rowItems = $();
+
+      $items
+        .each(function(k,v){
+          var $v = $(v);
+        
+          if( totalWidth<=( rowContentsWidth+$v.width() ) ){
+          
+            console.log( 'totalWidth<=( rowContentsWidth+$v.width() )' );
+            console.log( totalWidth+'<=( '+rowContentsWidth+'+'+$v.width()+' )' );
+            console.log( totalWidth+'<=( '+(rowContentsWidth+$v.width())+' )' );
+          
+            // Cannot add Current Item to Row - too wide.
+            // Process this row
+            var widthRemainder = totalWidth-rowContentsWidth+options.margin;
+            var perElement = Math.floor( options.margin+( widthRemainder/( $rowItems.length-1 ) ) );
+            var leftOver = widthRemainder-( perElement*( $rowItems.length-1 ) );
+            
+            $rowItems
+              .css({
+                display : 'inline-block' ,
+                marginRight : perElement
+              })
+              .last()
+                .css({
+                  marginRight : 0
+                });
+                
+            $rowItems = $();
+            rowContentsWidth = 0;
+          
+          }else{
+            console.log( 'More Space' );
+          }
+          
+          $rowItems = $rowItems.add( $v );
+          rowContentsWidth += $v.width()+options.margin;
+          
+          console.log( '$rowItems' , $rowItems );
+          console.log( 'rowContentsWidth' , rowContentsWidth );
+        
+        });
+        
+      if( rowContentsWidth!=0 ){
+      
+        // Remainder Elements
+        var widthRemainder = totalWidth-rowContentsWidth+options.margin;
+        var perElement = Math.floor( options.margin+( widthRemainder/( $rowItems.length-1 ) ) );
+        var leftOver = widthRemainder-( perElement*( $rowItems.length-1 ) );
+        
+        $rowItems
+          .css({
+            display : 'inline-block' ,
+            marginRight : perElement
+          })
+          .last()
+            .css({
+              marginRight : 0 ,
+              marginLeft : leftOver
+            });
+      
+      }
+
+    });
+
   };
-  var options = $.extend(defaults, options);
-
-  return this.each(function() {
-
-     var totalWidth = $(this).width();
-     var itemWidth = options.width? options.width : $(this).children(0).outerWidth();
-     var perRow = Math.floor(totalWidth/(itemWidth+options.margin));
-     var widthItemsOnRow = itemWidth*perRow;
-     var marginsWidth = totalWidth-widthItemsOnRow;
-
-     var newMargin = Math.floor(marginsWidth/(perRow-1))-4; // why is this -4 needed?!
-
-     $(this).children().each(function(index){
-
-       var m = newMargin;
-
-       if((index+1) % perRow == 0 && index !== 0) {
-         m = 0; // last in a row, dont give a margin.
-       }
-
-       $(this).css('margin-right', m + 'px').css('display', 'inline-block');
-     });
-  });
-
- };
 })(jQuery);
